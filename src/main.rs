c2b2402f072bf6791d68cc837176729a3b310ae6
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2025-05-07 13:56:32 +0900

    ➕ clapを導入した

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,25 @@
 mod file_reader;
 mod interpreter;
 mod token;
+use clap::Parser;
 use interpreter::Interpreter;
-use std::env;
-use std::process;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    filename: String,
+    input: Option<String>,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    if args.len() < 2 {
-        eprintln!("Usage: {} <filename> <input>", args[0]);
-        process::exit(1);
-    }
-
-    let filename = &args[1];
-    let input = if args.len() < 2 { Some(&args[2]) } else { None };
-
-    let program = file_reader::read(filename);
-    let input_content = match input {
-        Some(path) => file_reader::read(path),
+    let program = file_reader::read(&args.filename);
+    let input_content = match args.input {
+        Some(path) => file_reader::read(&path),
         None => "".to_string(),
     };
 
-    let interpreter = Interpreter::new(program.clone(), input_content.clone());
+    let interpreter = Interpreter::new(program, input_content);
     interpreter.interpret();
 }
