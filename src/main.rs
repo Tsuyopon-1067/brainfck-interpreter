da0604830b857613dfa8e00ab818b9108c41919c
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2025-05-07 14:02:53 +0900

    ✨ 入力はファイルではなく標準入力から取得するようにした

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod interpreter;
 mod token;
 use clap::Parser;
 use interpreter::Interpreter;
+use std::io::{self, Read, Write};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -15,11 +16,22 @@ fn main() {
     let args = Args::parse();
 
     let program = file_reader::read(&args.filename);
-    let input_content = match args.input {
-        Some(path) => file_reader::read(&path),
-        None => "".to_string(),
-    };
+
+    let input_flag = args.input.is_some();
+    let input_content = get_std_input(input_flag);
 
     let interpreter = Interpreter::new(program, input_content);
     interpreter.interpret();
+}
+
+fn get_std_input(input_flag: bool) -> String {
+    if input_flag {
+        print!("input: ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_to_string(&mut input).unwrap();
+        input.trim().to_string()
+    } else {
+        "".to_string()
+    }
 }
